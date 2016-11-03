581eadd59db5063707b394940f4fb4c7e6f8fc94
SINGH SOURABH BHAGWAN <sourabh@cse.iitb.ac.in>
2016-11-04 01:00:17 +0530

    Log in done

---
+++ lab11/Feeder27/app/src/main/java/com/feeder/audacity/feeder/Constants.java
@@ -0,0 +1,14 @@
+package com.feeder.audacity.feeder;
+
+/**
+ * Created by sourabh on 3/11/16.
+ */
+
+public class Constants {
+    public static String DOMAIN="http://192.168.0.134:8000/";
+    public static String LOGIN=DOMAIN+"feeder/studentlogin/";
+    public static int LOGINOK = 0;
+    public static int NETWORKERR = 1;
+    public static int PASSWORDINCORRECT = 2;
+    public static int NOTOKAY = 3;
+}
