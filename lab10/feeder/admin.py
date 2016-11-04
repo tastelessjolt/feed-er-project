1b9a2d22d618706659078cddffbbec84ae25438c
Harshith Goka <harshith9399@gmail.com>
2016-11-04 23:25:55 +0530

    Feedback view complete

--- lab10/feeder/admin.py
+++ lab10/feeder/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Instructor, Course, Student, Assignment
+from .models import *
 
 admin.site.register(Instructor)
 admin.site.register(Course)
 admin.site.register(Student)
 admin.site.register(Assignment)
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(RatingAnswer)
