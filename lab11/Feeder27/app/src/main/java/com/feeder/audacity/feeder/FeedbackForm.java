648fa7e54d2d9140991546347b4dccab1547e95e
Harshith Goka <harshith9399@gmail.com>
2016-11-05 14:30:17 +0530

    Feedform activity created

--- lab11/Feeder27/app/src/main/java/com/feeder/audacity/feeder/FeedbackForm.java
+++ lab11/Feeder27/app/src/main/java/com/feeder/audacity/feeder/FeedbackForm.java
@@ -8,6 +8,6 @@ public class FeedbackForm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback_form);
+        setContentView(R.layout.activity_feedback_form);    
     }
 }
